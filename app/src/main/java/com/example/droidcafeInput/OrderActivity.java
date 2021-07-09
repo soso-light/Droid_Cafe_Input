b6d2a1e38c5544038254e13c7897a5a70e103912
soso-light <80017031+soso-light@users.noreply.github.com>
2021-07-09 14:59:45 +0900

    radio button and texts

--- app/src/main/java/com/example/droidcafeInput/OrderActivity.java
+++ app/src/main/java/com/example/droidcafeInput/OrderActivity.java
@@ -3,6 +3,9 @@ package com.example.droidcafeInput;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -11,4 +14,27 @@ public class OrderActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
     }
+
+    public void displayToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton)view).isChecked();
+        switch (view.getId()){
+            case R.id.sameday:
+                if(checked) displayToast(getString(R.string.same_day_messenger_service));
+                break;
+            case R.id.nextday:
+                if(checked) displayToast(getString(R.string.next_day_ground_delivery));
+                break;
+            case R.id.pickup:
+                if(checked) displayToast(getString(R.string.pick_up));
+                break;
+            default: break;
+        }
+
+    }
+
 }
